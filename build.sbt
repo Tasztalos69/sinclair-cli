ddacc8c6cb2658e5628d2fe55f6718f29c633426
ApOszTroF <tasztalos69@gmail.com>
2020-08-09 17:14:09 +0200

    Finish getter/setter & Start addressing modes

--- build.sbt
+++ build.sbt
@@ -1,11 +1,15 @@
 name := "sinclair-cli"
 
+maintainer := "bmk@jelszo.co"
+
 version := "0.1"
 
 scalaVersion := "2.13.3"
 
 libraryDependencies ++= Seq(
   "org.rogach" %% "scallop" % "3.5.0",
-  "io.spray" %%  "spray-json" % "1.3.5",
+  "io.spray" %% "spray-json" % "1.3.5",
   "commons-codec" % "commons-codec" % "1.14"
 )
+
+enablePlugins(JavaAppPackaging)
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,5 @@
 import java.io.{File, FileNotFoundException, PrintWriter}
-import java.net.{DatagramPacket, DatagramSocket, InetSocketAddress, SocketTimeoutException}
+import java.net._
 
 import javax.crypto.Cipher
 import javax.crypto.spec.SecretKeySpec
@@ -10,21 +10,22 @@ import spray.json._
 
 import scala.io.Source
 import scala.io.StdIn.readLine
+import scala.jdk.CollectionConverters._
 import scala.util.control.Breaks._
 
 // CLI Syntax constructor
 
 class Conf(arguments: Seq[String]) extends ScallopConf(arguments) {
-  version("v1.0.0 | MIT | Kiss Benedek Mate")
+  version("v1.0.0 | MIT | Kiss Benedek Máté")
   val search = new Subcommand("search", "s") {
 
   }
-  val get = new Subcommand("get", "g") {
-    val help = opt[Boolean]()
-    val prop = trailArg[String]()
+  val ac = new Subcommand("ac") {
+    val prop = trailArg[String]("property", "The name of the requested key.", required = false)
+    val value = trailArg[String]("value", "The value to set the AC to.", required = false)
   }
   addSubcommand(search)
-  addSubcommand(get)
+  addSubcommand(ac)
   verify()
 }
 
@@ -47,10 +48,26 @@ object Implicits {
 import Implicits._
 
 object Main {
-  val testArgs: Array[String] = Array("search")
   val GENERIC_KEY: String = "a3K8Bx%2r8Y7#xDh"
 
   // Helper FNs
+
+  def getDefaultBroadcast: Option[String] = {
+    val interfaces = NetworkInterface.getNetworkInterfaces
+    while (interfaces.hasMoreElements) {
+      val itf = interfaces.nextElement()
+      if (!itf.isLoopback) {
+        val addresses = itf.getInterfaceAddresses.asScala
+        for (addr: InterfaceAddress <- addresses) {
+          val bc = addr.getBroadcast
+          if (bc != null) {
+            return Some(bc.toString.substring(1))
+          }
+        }
+      }
+    }
+    None
+  }
 
   def keyToSpec(key: String): SecretKeySpec = {
     var keyBytes = Array.fill[Byte](1024)(0)
@@ -132,7 +149,133 @@ object Main {
     }
   }
 
+  // CLI functions
+
+  def search(config: JsObject): Unit = {
+    if (config.fields.contains("devices")) {
+      print("Warning: there are previously bound devices. Do you want to overwrite them? (y/N) ")
+      val resp = readLine()
+      if (resp.toUpperCase != "Y" && resp.toUpperCase != "YES") {
+        println("Aborting.")
+        System.exit(0)
+      }
+    }
+    if (!config.fields.contains("broadcastAddress")) {
+      println("Searching for default broadcast address...\n(Alternatively, you can set the broadcast address with scli config broadcast <ip>)")
+      val bc = getDefaultBroadcast.getOrElse("")
+      if (bc.isEmpty) {
+        println("Error: cannot get default broadcast address.")
+        System.exit(1)
+      } else {
+        println(s"Found broadcast address $bc, and setting it as default.")
+        val pw = new PrintWriter(new File("config.json"))
+        val newConfig = JsObject(config.fields + ("broadcastAddress" -> bc.toJson))
+        pw.write(newConfig.prettyPrint)
+        pw.close()
+      }
+    }
+    println("Searching...")
+    val msg = """{"t":"scan"}""".getBytes()
+    val addr = new InetSocketAddress(config.fields("broadcastAddress").toString.stripQ, 7000)
+    val p = new DatagramPacket(msg, msg.length, addr)
+    val s = new DatagramSocket()
+    s.setSoTimeout(5000)
+    s.setBroadcast(true)
+    s.setReuseAddress(true)
+    s.send(p)
+
+    var results: Array[SearchResult] = Array()
+
+    breakable {
+      while (true) {
+        val in = Array.fill[Byte](1024)(0)
+        val recv = new DatagramPacket(in, in.length)
+        try {
+          s.receive(recv)
+          if (recv.getLength > 0) {
+            val jsonRecv = JsonParser(new String(recv.getData, 0, recv.getLength))
+            val recvMap = jsonRecv.convertTo[Map[String, JsValue]]
+            val decrypted = decrypt_generic(recvMap("pack").toString().stripQ)
+            val jsonDecrypted = JsonParser(new String(decrypted, 0, decrypted.length))
+            val decryptedMap = jsonDecrypted.convertTo[Map[String, JsValue]]
+            var addr = recv.getAddress.toString
+            if (addr.startsWith("/")) addr = addr.substring(1)
+            results = results :+ SearchResult(addr, recv.getPort, decryptedMap("cid").toString, if (decryptedMap.contains("name")) decryptedMap("name").toString else "Unknown")
+          }
+        } catch {
+          case _: SocketTimeoutException =>
+            println(s"Found ${results.length} devices.")
+            break()
+        }
+      }
+    }
+    if (results.length == 0) {
+      System.exit(0)
+    }
+    var binds: Array[Map[String, String]] = Array()
+    for (r <- results) {
+      binds = binds :+ bind(r)
+    }
+    if (binds.length == 1) {
+      println("Setting the device as default...")
+      binds(0) = binds(0) + ("default" -> "true")
+    } else {
+      println("Found multiple devices. To use the shorthand command (without specifying a device), use: scli config default <id>")
+    }
+    val pw = new PrintWriter(new File("config.json"))
+    val newConfig = JsObject(config.fields + ("devices" -> binds.toJson))
+    pw.write(newConfig.prettyPrint)
+    pw.close()
+    println(s"Saved ${binds.length} devices.")
+
+  }
+
+  def getParam(prop: ScallopOption[String]): String = prop.getOrElse("") match {
+    case "power" => "Pow"
+    case "mode" => "Mod"
+    case "temp" => "SetTem"
+  }
+
+  def getValue(value: ScallopOption[String]): Int = value.getOrElse("") match {
+    case "on" => 1
+    case "off" => 0
+    case "auto" => 0
+    case "cool" => 1
+    case "dry" => 2
+    case "fan" => 3
+    case "heat" => 4
+  }
+
+  def ac(config: JsObject, prop: ScallopOption[String], value: ScallopOption[String]): Unit = {
+    val device = config.fields("devices").convertTo[Array[Map[String, String]]].filter(p => p("default") == "true")(0)
+    var setMode = false
+    if (value.isDefined) setMode = true
+    val param = getParam(prop)
+    var pack: String = ""
+    var numValue: Int = -1
+    if (setMode) {
+      numValue = getValue(value)
+      pack = s"""{"opt":["$param"],"p":[$numValue],"t":"cmd"}"""
+    } else {
+      pack = s"""{"cols":["$param"],"mac":"${device("id")}","t":"status"}"""
+    }
+    val pack_encrypted = encrypt(pack, device("key"))
+
+    val request = s"""{"cid":"app","i":0,"pack":"$pack_encrypted","t":"pack","tcid":"${device("id")}","uid":0}"""
+    val recv = sendData(device("ip"), 7000, request.getBytes)
+    val jsonRecv = JsonParser(new String(recv.getData, 0, recv.getLength))
+    val mapRecv = jsonRecv.convertTo[Map[String, JsValue]]
+    if (mapRecv("t").toString == """"pack"""") {
+      val decrypted = decrypt(mapRecv("pack").toString().stripQ, device("key"))
+      val jsonDecrypted = JsonParser(new String(decrypted, 0, decrypted.length))
+      val mapDecrypted = jsonDecrypted.convertTo[Map[String, JsValue]]
+      println(mapDecrypted)
+    }
+  }
+
   // Main
+
+  val testArgs: Array[String] = Array("ac", "power", "off")
 
   def main(args: Array[String]): Unit = {
     val conf = new Conf(testArgs)
@@ -144,74 +287,12 @@ object Main {
     } catch {
       case _: FileNotFoundException =>
         val pw = new PrintWriter(new File("config.json"))
-        pw.write("{}");
+        pw.write("{}")
         pw.close()
     }
 
-    if (conf.subcommand.contains(conf.search)) {
-      if (config.fields.contains("devices")) {
-        print("Warning: there are previously bound devices. Do you want to overwrite them? (y/N) ")
-        val resp = readLine()
-        if (resp.toUpperCase != "Y" && resp.toUpperCase != "YES") {
-          println("Aborting.")
-          System.exit(0)
-        }
-      }
-      println("Searching...")
-      val msg = """{"t":"scan"}""".getBytes()
-      val addr = new InetSocketAddress("192.168.0.255", 7000)
-      val p = new DatagramPacket(msg, msg.length, addr)
-      val s = new DatagramSocket()
-      s.setSoTimeout(5000)
-      s.setBroadcast(true)
-      s.setReuseAddress(true)
-      s.send(p)
+    if (conf.subcommand.contains(conf.search)) search(config)
 
-      var results: Array[SearchResult] = Array()
-
-      breakable {
-        while (true) {
-          val in = Array.fill[Byte](1024)(0)
-          val recv = new DatagramPacket(in, in.length)
-          try {
-            s.receive(recv)
-            if (recv.getLength > 0) {
-              val jsonRecv = JsonParser(new String(recv.getData, 0, recv.getLength))
-              val recvMap = jsonRecv.convertTo[Map[String, JsValue]]
-              val decrypted = decrypt_generic(recvMap("pack").toString().stripQ)
-              val jsonDecrypted = JsonParser(new String(decrypted, 0, decrypted.length))
-              val decryptedMap = jsonDecrypted.convertTo[Map[String, JsValue]]
-              var addr = recv.getAddress.toString
-              if (addr.startsWith("/")) addr = addr.substring(1)
-              results = results :+ SearchResult(addr, recv.getPort, decryptedMap("cid").toString, if (decryptedMap.contains("name")) decryptedMap("name").toString else "Unknown")
-            }
-          } catch {
-            case _: SocketTimeoutException =>
-              println(s"Found ${results.length} devices.")
-              break()
-          }
-        }
-      }
-      if (results.length == 0) {
-        System.exit(0)
-      }
-      var binds: Array[Map[String, String]] = Array()
-      for (r <- results) {
-        binds = binds :+ bind(r)
-      }
-      if (binds.length == 1) {
-        println("Setting the device as default...")
-        binds(0) = binds(0) + ("default" -> "true")
-      } else {
-        println("Found multiple devices. To use the shorthand command (without specifying a device), use: scli config default <id>")
-      }
-      val pw = new PrintWriter(new File("config.json"))
-      config = JsObject(config.fields + ("devices" -> binds.toJson))
-      pw.write(config.prettyPrint)
-      pw.close()
-      println(s"Saved ${binds.length} devices.")
-
-    }
-    if (conf.subcommand.contains(conf.get)) println("get supplied")
+    if (conf.subcommand.contains(conf.ac)) ac(config, conf.ac.prop, conf.ac.value)
   }
 }
